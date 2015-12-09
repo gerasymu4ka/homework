05961f0b9c2af62419f2d7a37d83f1008a993614
gera <nadsca@gmail.com>
2015-12-09 20:47:26 +0200

    corrections from the lecture

--- vector2d.py
+++ vector2d.py
@@ -9,28 +9,50 @@
 #abs(a) == 5
 #No "strange" operations should be allowed
 
+from math import hypot
+
 class Vector2D():
         def __init__(self, x, y):
-                self.x = x
-                self.y = y
+                self._x = x
+                self._y = y
 
         def __repr__(self):
-                return 'Vector2D({}, {})'.format(self.x, self.y)
+                return 'Vector2D({}, {})'.format(self._x, self._y)
         def __add__(self, other):
-                x = self.x + other.x
-                y = self.y + other.y
-                return Vector2D(x, y)
+                if not isinstance(other, Vector2D):
+                        return NotImplemented
+                return Vector2D(self._x + other._x, self._y + other._y)
+        def __eq__(self, other):
+                if not isinstance(other, Vector2D):
+                        return NotImplemented
+                return self._x == other._x and self._y == other._y
         def __neg__(self):
-                return Vector2D(-self.x, -self.y)
+                return Vector2D(-self._x, -self._y)
         def __pow__(self, num):
-                x = self.x ** num
-                y = self.y ** num
+                x = self._x ** num
+                y = self._y ** num
                 return Vector2D(x, y)
         def __abs__(self):
-                return (self.x**2 + self.y**2)**0.5
+                return hypot(self._x, self._y)
+        #(self._x**2 + self._y**2)**0.5
+        
+        def __mul__(self, other):
+                if not isinstance(other, (int, float)):
+                        return NotImplemented
+                return Vector2D(self._x * other, self._y * other)
+        def __rmul__(self, other):
+                return self * other
+        @property
+        def x(self):
+                return self._x
+        @property
+        def y(self):
+                return self._y
 
 a = Vector2D(3, 4)
 b = Vector2D(8, -5)
 c = Vector2D(0, 4)
+
+a.x, a.y
 
 
